ae03de23795c18ad8d9f880a0b3010ede707a6ea
Matt Goodhart <mgoodhart@google.com>
2022-04-28 20:21:20 -0400

    second push

--- WT Practice/GameBoardViewController.swift
+++ WT Practice/GameBoardViewController.swift
@@ -7,17 +7,44 @@
 
 import UIKit
 
-let endpointString: String = "https://namegame.willowtreeapps.com/api/v1.0/profiles"
+class GameBoardViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let endpointString: String = "https://namegame.willowtreeapps.com/api/v1.0/profiles"
 
-class GameBoardViewController: UIViewController {
+    let gameType : GameType = .practice
+    
+    var nameInQuestion: String = ""
+    
+    var modelForGame: [EmployeeResults] = []
+    
+    let timerSeconds: Int = 20
+    
+    var score: Int = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        
         
         // query and parse the json while showing activity indicator and disabling buttons
-        
+        setUpGameType()
     }
     
+    @objc func gameTimer() {
+        //
+    }
+    
+    func setUpGameType() {
+        if gameType == .practice {
+            self.title = "Practice Mode"
+        } else {
+            self.title = "Timed Mode"
+        }
+    }
     
     func fetchEmployeeInfo() {
         guard let endpoint = URL(string: endpointString) else {
@@ -28,6 +55,30 @@ class GameBoardViewController: UIViewController {
         Networking.shared.fetchAndParseJSON(url: endpoint)
         
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+      //process game logic based on game type
+        switch gameType {
+        case .practice:
+            if nameInQuestion == (self.modelForGame[(indexPath).row].firstName + " " + self.modelForGame[(indexPath).row].lastName) {
+                //unhide the "correct" image
+            } else {
+                //unhide the "incorrect" image
+                //present game over mesage
+            }
+        case .timed:
+            let timer = Timer.scheduledTimer(timeInterval: 20.0, target: self, selector: #selector(gameTimer), userInfo: nil, repeats: false)
+        }
     }
 }
 
--- WT Practice/HomeViewController.swift
+++ WT Practice/HomeViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 
 enum GameType {
-    case practce
+    case practice
     case timed
 }
 
